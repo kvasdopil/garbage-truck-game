6bb2086083d1a105c999a9389d2d6eb606d6c22d
Alexey Guskov <kvasdopil@gmail.com>
2025-09-18 12:10:20 +0200

    fix: remove tf, add gh action

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: '/',
+  base: '/garbage/',
   server: {
     host: true,
     allowedHosts: ['localhost', '127.0.0.1', '.ngrok-free.app'],
