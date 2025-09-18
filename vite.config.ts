ef7ebface1452740f71835b0cd5c272de80083b2
Alexey Guskov <kvasdopil@gmail.com>
2025-09-18 12:11:24 +0200

    fix: prefix

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: '/garbage/',
+  base: '/garbage-truck-game/',
   server: {
     host: true,
     allowedHosts: ['localhost', '127.0.0.1', '.ngrok-free.app'],
